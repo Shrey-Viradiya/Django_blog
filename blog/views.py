c795f96abede73f30b0620583ca7670700c0de15
Shrey Viradiya <shrey.viradiya.100@outlook.com>
2019-08-12 16:34:39 +0530

    Video 5

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'Shrey Viradiya',
-        'title' : 'Django is Cool',
-        'content' : 'I am using Django',
-        'date_posted' : '10 August, 2019'
-    },
-    {
-        'author' : 'Betty Cooper',
-        'title' : 'Riverdale',
-        'content' : 'Where is blackhood?',
-        'date_posted' : '11 August, 2019'
-    }
-
-]
-
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
