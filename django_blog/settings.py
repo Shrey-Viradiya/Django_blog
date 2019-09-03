61ca6b07b75065e8af3c5053eff6d38e5b1a56d1
Shrey Viradiya <shrey.viradiya.100@outlook.com>
2019-09-03 23:28:08 +0530

    deploy change

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
@@ -145,8 +145,3 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
